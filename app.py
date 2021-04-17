0c93297ebec4523a10dd961a735890e88261993b
Roman Solovev <RASolovyev@sberbank.ru>
2021-04-18 01:51:18 +0300

    + connected web3 to local network for sample function

--- app.py
+++ app.py
@@ -9,12 +9,21 @@ app = Flask(__name__)
 def select_account():
     with open('accounts.json') as accounts_file:
         accounts = json.load(accounts_file)
-        addresses = []
-        for acc in accounts.items():
-            addresses.append(acc[1]["address"])
+    addresses = []
+    for acc in accounts.items():
+        addresses.append(acc[1]["address"])
     return render_template('select_account.html', addresses=addresses)
 
 
 @app.route('/<account>')
 def dashboard(account):
-    return render_template('dashboard.html', account=account)
+    w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+    with open('accounts.json') as accounts_file:
+        accounts = json.load(accounts_file)
+    priv_key = ""
+    for acc in accounts.items():
+        if acc[1]["address"] == account:
+            priv_key = acc[1]["private_key"]
+    account = w3.eth.account.privateKeyToAccount(priv_key)
+    balance = int(w3.eth.getBalance(account.address))/10**18
+    return render_template('dashboard.html', account=account.address, balance=balance)
